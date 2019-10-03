247f3df3ff031fcf746133f33a8330d8fb89ed54
peace <astonreba@gmail.com>
2019-10-03 10:39:29 +0300

    Make changes in file

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,36 +1,35 @@
 import React, { Component } from 'react';
 import Square from './Square';
 
-class Board extends Component {
+export default class Board extends Component {
   renderSquare(i) {
     return (
-      <Square 
+      <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
     );
   }
+
   render() {
     return (
       <div>
-        <div className="border-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}    
-            {this.renderSquare(3)}       
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
-        <div className="border-row">
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}    
-            {this.renderSquare(6)}       
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
-        <div className="border-row">
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}    
-            {this.renderSquare(9)}       
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
   }
 }
-
-export default Board;
